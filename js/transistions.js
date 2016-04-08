9a4ab9a7c25c4c2b4836965a03feb28f018a48dc
Alfredauyeung <alfredsdesign@gmail.com>
2016-04-08 12:26:55 -0400

    updates

---
+++ js/transistions.js
@@ -0,0 +1,23 @@
+
+		setInterval(animateMyShit,  4000);
+
+		function animateMyShit () {
+    		$('#image1').fadeToggle(3000);
+    		$('#image2').fadeToggle(3000);	
+		}
+
+
+
+
+
+	// $(document).ready(function() {
+ //        var keys = [];
+ //        var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+ //        $(document).keydown(function(keyEvent) {
+ //          keys.push(keyEvent.keyCode);
+ //          if ( keys.length > code.length ) { keys.shift(); }
+ //          if ( keys.toString() == code.toString() ) {
+ //            window.open("http://www.aauyeung.com/secondlayer/");
+ //          }
+ //        });
+ //      });
