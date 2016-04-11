a6aef38a6fbc62547c274171702ebcf0cab2a4ec
Alfredauyeung <alfredsdesign@gmail.com>
2016-04-11 12:55:38 -0400

    tail

--- dog/js/javascript.js
+++ dog/js/javascript.js
@@ -1,3 +1,6 @@
-$("#belly").click(function() {
+// $(document).ready(function (){
+// 	$('#tail').rotate({ endDeg: 90, duration: .5 });
+// })
+$("#belly").click(function() {					/*change the face of doge*/
     $(this).find('img').toggle();
 });
