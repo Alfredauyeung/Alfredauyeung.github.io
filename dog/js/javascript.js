818a63221e22e99251cbb9f99a9a777514a8b21f
Alfredauyeung <alfredsdesign@gmail.com>
2016-04-18 00:12:40 -0400

    roomi

--- dog/js/javascript.js
+++ dog/js/javascript.js
@@ -1,28 +1,6 @@
-// $(document).ready(function (){
-// 	$('#tail').rotate({ endDeg: 90, duration: .5 });
-// }
-// $(document).ready(function (){
-// 	setInterval(function(){
-// 		$('#belly').animate({
-// 			marginTop: "50px"
-// 		}, 1000)
-// 		$('#belly').animate({
-// 			marginTop: "-=50px"
-// 		}, 1000)
-// 	}, 2000);
-// })
-
 $("#belly").click(function() {					/*change the face of doge*/
     $(this).find('.doge').toggle();
 });
-
-$('#card').on('click', function() {
-	$(this).css({"border-color": "red",
-					"border-width": "1px",
-					"border-style": "solid"
-
-	});
-})
 
 // $(document).ready(function (){
 // 	$('#tail').rotate({
--- js/transistions.js
+++ js/transistions.js
@@ -1,22 +1,15 @@
-		setInterval(animateMyShit,  4000);
+		setInterval(animateMyShit, 4000);
 
 		function animateMyShit () {
     		$('#image1').fadeToggle(3000);
     		$('#image2').fadeToggle(3000);	
 		}
 
+		// setInterval(roomiSlide, 4500);
 
-
-
-
-	// $(document).ready(function() {
- //        var keys = [];
- //        var code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
- //        $(document).keydown(function(keyEvent) {
- //          keys.push(keyEvent.keyCode);
- //          if ( keys.length > code.length ) { keys.shift(); }
- //          if ( keys.toString() == code.toString() ) {
- //            window.open("http://www.aauyeung.com/secondlayer/");
- //          }
- //        });
- //      });
+		// function roomiSlide () {
+  //   		$('#r1').fadeToggle(1500);
+  //   		$('#r2').fadeToggle(1500);
+  //   		$('#r3').fadeToggle(1500);
+  //   		$('#r4').fadeToggle(1500);	
+		// }
