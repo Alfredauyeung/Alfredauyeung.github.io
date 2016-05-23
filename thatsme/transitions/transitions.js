77e986c08ad37745a14912fd8784b7a6350ca31f
Alfredauyeung <alfredsdesign@gmail.com>
2016-05-23 13:10:57 -0400

    added interaction

--- thatsme/transitions/transitions.js
+++ thatsme/transitions/transitions.js
@@ -1,6 +1,10 @@
-// $(document).ready(function() {
+$(document).ready(function() {
+	$('.front')
+			.animate({marginTop: "5px"}, 200)
+			.animate({marginTop: "0px"}, 200);
+})
 
-// 		$('.front')
-// 			.animate({marginTop: "5px"}, 200)
-// 			.animate({marginTop: "0px"}, 200);
-// 		})
+setInterval(supApp, 270);
+	function supApp () {
+		$('.sup').show();
+}
